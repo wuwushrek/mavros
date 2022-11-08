48037f52a94426f610801fedf0af55bd746c173c
Franck Djeumou <frmbouwe@gmail.com>
2022-11-08 10:54:43 -0600

    mpc motor command messages and plugins

---
+++ mavros_extras/src/plugins/mpc_motors_cmd.cpp
@@ -0,0 +1,71 @@
+/**
+ * @brief mpc_motors_cmd plugin
+ * @file mpc_motors_cmd.cpp
+ * @author Franck Djeumou <frmbouwe@gmail.com>
+ *
+ * @addtogroup plugin
+ * @{
+ */
+/*
+ * Copyright 2022 Franck Djeumou
+ *
+ */
+
+#include <mavros/mavros_plugin.h>
+#include <boost/algorithm/string.hpp>
+
+#include <mavros_msgs/MPCMotorsCMD.h>
+
+namespace mavros {
+namespace extra_plugins {
+
+/**
+ * @brief MPCMotorsCMD plugin
+ *
+ * Publishes motors command to the FCU
+ */
+class MPCMotorsCMDPlugin : public plugin::PluginBase {
+public:
+	MPCMotorsCMDPlugin() : PluginBase(),
+		mpc_motors_cmd_nh("~mpc_motors_cmd")
+	{ }
+
+	void initialize(UAS &uas_) override
+	{
+		PluginBase::initialize(uas_);
+		// Subscribers
+        mpc_motors_cmd_sub = mpc_motors_cmd_nh.subscribe("cmd", 2, &MPCMotorsCMDPlugin::mpc_motors_cmd_cb, this);
+	}
+
+	Subscriptions get_subscriptions() override
+	{
+		return { /* Rx disabled */ };
+	}
+
+private:
+	ros::NodeHandle mpc_motors_cmd_nh;			//!< node handler
+    ros::Subscriber mpc_motors_cmd_sub;        //!< mavros_msgs/MPCMotorsCMD subscriber
+
+    void mpc_motors_cmd_cb(const mavros_msgs::MPCMotorsCMD::ConstPtr &req)
+    {
+       mavlink::common::msg::MPC_MOTORS_CMD msg{};
+       msg.time_usec = req->time_usec; 
+       msg.time_init = req->time_init;
+       msg.dt = req->dt;
+       for (int i = 0; i < mavros_msgs::MPCMotorsCMD::HORIZON_MPC; i++) {
+           msg.m1[i] = req->m1[i];
+           msg.m2[i] = req->m2[i];
+           msg.m3[i] = req->m3[i];
+           msg.m4[i] = req->m4[i];
+       }
+       UAS_FCU(m_uas)->send_message_ignore_drop(msg);
+       // Warn the message was sent
+    //    ROS_INFO("MPC_MOTORS_CMD message sent");
+    }
+};
+
+}	// namespace extra_plugins
+}	// namespace mavros
+
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(mavros::extra_plugins::MPCMotorsCMDPlugin, mavros::plugin::PluginBase)
